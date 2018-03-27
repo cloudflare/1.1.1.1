a1979e68d943503c199bc999ca024b60a0061d67
Teffen Ellis <teffen@cloudflare.com>
2018-03-26 19:13:03 -0700

    Refactor landing. Flesh out instructions.

--- source/pages/index.ts
+++ source/pages/index.ts
@@ -4,24 +4,80 @@ import platform from 'platform'
 import SweetScroll from 'sweet-scroll'
 import { logoBanner } from './console'
 
-function elementByRef(ref: string): HTMLElement {
+interface DeviceInfo {
+  id: string
+  label: string
+}
+
+const deviceInfo: { [index: string]: DeviceInfo } = {
+  'Windows': {
+    label: 'Windows',
+    id: 'windows'
+  },
+  'OS X': {
+    label: 'MacOS',
+    id: 'mac-os'
+  },
+  'iOS': {
+    label: 'iOS',
+    id: 'iphone'
+  },
+  'Android': {
+    label: 'Android',
+    id: 'android'
+  }
+}
+
+function ref(ref: string): HTMLElement {
   return <HTMLElement>document.querySelector(`[data-ref="${ref}"]`)!
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log(logoBanner)
-  console.log(window.btoa('Join Cloudflare and help build a better Internet https://cloudflare.com/careers?utm=1.1.1.1-DNS'))
+  // console.log(logoBanner)
+  // console.log(window.btoa('Join Cloudflare and help build a better Internet https://cloudflare.com/careers?utm=1.1.1.1-DNS'))
+
   const scroller = new SweetScroll()
 
-  const deviceLabel = elementByRef('deviceLabel')
-  deviceLabel.textContent = deviceLabel.textContent!.replace('{{device}}', platform.product || 'device')
+  const $el = {
+    instructionPicker: ref('instructionPicker'),
+    instructionChoices: Array.prototype.slice.call(ref('instructionPicker').querySelectorAll('.choice')),
+    deviceLabel: ref('deviceLabel'),
+    slideshow: ref('slideshow'),
+    setupSection: ref('setup')
+    // instructionsContent: ref('instructionsContent'),
+    // setupButton: ref('setupButton'),
+    // infoButton: ref('infoButton')
+  }
 
-  const slideshow = elementByRef('slideshow')
-  const slideCount = slideshow.querySelectorAll('.background-slide').length
+  // $el.infoButton.addEventListener('click', )
 
-  window.setInterval(() => {
-    const slideIndex = parseInt(slideshow.dataset.activeSlide!, 10)
+  function chooseInstructions (platform: string) {
+    $el.instructionChoices.forEach((choice: HTMLElement) => {
+      choice.classList.toggle('selected', choice.dataset.platform === platform)
+    })
 
-    slideshow.dataset.activeSlide = (slideIndex > slideCount - 1 ? 1 : slideIndex + 1).toString()
-  }, 3000)
+    $el.setupSection.dataset.platform = platform
+    $el.deviceLabel.textContent = $el.deviceLabel.dataset.label!.replace('{{device}}', device.label)
+  }
+
+  $el.instructionChoices.forEach((choice: HTMLElement) => {
+    choice.addEventListener('click', chooseInstructions.bind(null, choice.dataset.platform))
+  })
+
+  let device: DeviceInfo = deviceInfo['OS X']
+
+  if (platform.os && platform.os.family && deviceInfo.hasOwnProperty(platform.os.family)) {
+    device = deviceInfo[platform.os.family]
+  }
+
+
+  chooseInstructions(device.id)
+
+  // const slideCount = $el.slideshow.querySelectorAll('.background-slide').length
+
+  // window.setInterval(() => {
+  //   const slideIndex = parseInt($el.slideshow.dataset.activeSlide!, 10)
+
+  //   $el.slideshow.dataset.activeSlide = (slideIndex > slideCount - 1 ? 1 : slideIndex + 1).toString()
+  // }, 3000)
 })
