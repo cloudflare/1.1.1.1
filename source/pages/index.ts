6c601095a7cf47992c2a034f1805833965204cde
Teffen Ellis <teffen@cloudflare.com>
2018-03-31 00:47:39 -0700

    Enable dropdown toggle.

--- source/pages/index.ts
+++ source/pages/index.ts
@@ -68,7 +68,14 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   $el.languageSelector.addEventListener('click', () => {
-    $el.languageSelector.classList.toggle('dropdown-open')
+  })
+
+  document.addEventListener('click', (event) => {
+    if (event.target === $el.languageSelector || $el.languageSelector.contains(<Node>event.target)) {
+      $el.languageSelector.classList.toggle('dropdown-open')
+    } else {
+      $el.languageSelector.classList.toggle('dropdown-open', false)
+    }
   })
 
   function chooseInstructions (platformId: string) {
